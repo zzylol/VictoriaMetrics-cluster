f741c3f105b2cceca93e9e5ac48ca52e06bd55da
Zeying Zhu <zeyingz@umd.edu>
2025-01-11 23:05:38 -0500

    update

---
+++ kill_cluster.sh
@@ -0,0 +1,5 @@
+pkill -9 vmstorage
+pkill -9 vmselect
+pkill -9 vminsert
+pkill -9 vmsketch
+tmux kill-server
--- start_cluster.sh
+++ start_cluster.sh
@@ -1,4 +1,4 @@
-./bin/vmstorage -storageDataPath=./ -retentionPeriod=100d
-./bin/vmsketch 
-./bin/vmselect -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500
-./bin/vminsert -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500
+tmux new-session -d -s "vmstorage1" ./bin/vmstorage -storageDataPath=./ -retentionPeriod=100d
+tmux new-session -d -s "vmsketch1" ./bin/vmsketch 
+tmux new-session -d -s "vmselect1" ./bin/vmselect -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500
+tmux new-session -d -s "vminsert1" ./bin/vminsert -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500
