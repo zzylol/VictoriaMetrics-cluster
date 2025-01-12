be96bb5504e045d5c400cb60749c56e53aa00f78
Zeying Zhu <zeyingz@umd.edu>
2025-01-11 23:15:12 -0500

    udapte

--- start_cluster.sh
+++ start_cluster.sh
@@ -1,4 +1,4 @@
 tmux new-session -d -s "vmstorage1" ./bin/vmstorage -storageDataPath=./ -retentionPeriod=100d
 tmux new-session -d -s "vmsketch1" ./bin/vmsketch 
-tmux new-session -d -s "vmselect1" ./bin/vmselect -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500 -search.maxQueryDuration==600m
+tmux new-session -d -s "vmselect1" ./bin/vmselect -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500  
 tmux new-session -d -s "vminsert1" ./bin/vminsert -storageNode=127.0.0.1:8400 -sketchNode=127.0.0.1:8500
